43c129ed98400f6f56c23e29455d7621972b797f
Felix Frohboese <felix.frohboese@gmail.com>
2022-08-05 15:09:31 +0200

    Loop script

--- job.sh
+++ job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-python3 ingest_cmc_data.py
-
-python3 pause.py
-
-python3 transform_cmc_data.py
-
-python3 pause.py
+for (( ; ; ))
+do
+  python3 ingest_cmc_data.py
+  python3 pause_5sec.py
+  python3 transform_cmc_data.py
+  python3 pause_55sec.py
+done
