34896df00e42b5f9a1502fc8840d6addf71fab6b
Felix Frohboese <felix.frohboese@gmail.com>
2022-08-17 13:16:39 +0200

    Shell job adjustments

--- job.sh
+++ job.sh
@@ -2,7 +2,7 @@
 for (( ; ; ))
 do
   python3 ingest_cmc_data.py
-  python3 pause_5sec.py
+  python3 pause_short.py
   python3 transform_cmc_data.py
-  python3 pause_55sec.py
+  python3 pause_long.py
 done
